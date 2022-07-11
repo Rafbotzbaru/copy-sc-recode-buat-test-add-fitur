674801f4134a4e9f9d919a6164782b65e5c47d53
Rafbotzbaru <108472722+Rafbotzbaru@users.noreply.github.com>
2022-07-11 18:59:11 +0700

    Update scriptbot.js

--- command/info/scriptbot.js
+++ command/info/scriptbot.js
@@ -10,7 +10,7 @@ module.exports = {
 				image: { url: config.thumb },
 				footer: config.namebot,
 				// Gausah di ubah kontol najis modal copas sana sini ubah source cih
-				caption: `Script Bot Is here\ndon't forget fork + star XD`,
+				caption: `Script Bot Ada Di Owner ,Klok Mau Gratis/Beli Yg /Enc/No Enc Chat Owner`,
 				templateButtons: [
 					{ urlButton: { displayText: "Script Bot", url: "https://wa.me/6281946945315?text=bg+gw+mau+beli+sc+recodenya+abg+yg+no+enc" } },
 				],
